922a34aeba85cfd716e844f444ae5bf55f4a48c0
kubomi-3 <yfhyfh0@gmail.com>
2021-01-11 15:19:25 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,         presence: true
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :last_name,        presence: true
@@ -17,6 +17,6 @@ class User < ApplicationRecord
     validates :last_name_kana,        presence: true
     validates :first_name_kana,       presence: true
   end
-  
-  validates :birthday,         presence: true
+
+  validates :birthday, presence: true
 end
