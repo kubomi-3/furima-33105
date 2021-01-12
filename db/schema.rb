1b5b0db4eb468849754ae2b5e4da6d6791cb0f15
kubomi-3 <yfhyfh0@gmail.com>
2021-01-12 15:48:10 +0900

    itemsテーブルのカラム名の変更

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_074327) do
     t.integer "condition_id", null: false
     t.integer "delivery_fee_id", null: false
     t.integer "area_id", null: false
-    t.integer "days_id", null: false
+    t.integer "day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
