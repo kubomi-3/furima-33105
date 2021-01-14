de32420688a3ad6cc8a20ea0b92a9b0542f2e340
kubomi-3 <yfhyfh0@gmail.com>
2021-01-14 13:11:35 +0900

    newアクションとcreateアクションの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,28 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_fee_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
