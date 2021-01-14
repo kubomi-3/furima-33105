06d4957205f1bbe227f5494f49f3cb4a2bb0d5d3
kubomi-3 <yfhyfh0@gmail.com>
2021-01-14 15:07:23 +0900

    itemモデルのpresence: trueをまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,9 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :day
 
-  validates :name, :description, :image, presence: true
   validates :category_id, :condition_id, :delivery_fee_id, :area_id, :day_id, numericality: { other_than: 1 }
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options presence: true do
+    validates :name, :description, :image
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 end
