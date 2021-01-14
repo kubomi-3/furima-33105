6b6e271ed67b06b2ce3a90bf623c3a4d7db9879d
kubomi-3 <yfhyfh0@gmail.com>
2021-01-14 13:12:00 +0900

    itemモデルのバリデーションとアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+
+  has_one :order
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :area
+  belongs_to :day
+
+  validates :name, :description, :image,  presence: true
+  validates :category_id, :condition_id, :delivery_fee_id, :area_id, :day_id, numericality: { other_than: 1 }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  
 end
