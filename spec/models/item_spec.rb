ed927c29f131bc732e98df1b3b54e2bc19b2d787
kubomi-3 <yfhyfh0@gmail.com>
2021-01-14 16:34:04 +0900

    contextによる場合分けと下から2つのexampleの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,68 +6,82 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品の出品' do
-    it '情報を正しく入力すれば登録できる' do
-      expect(@item).to be_valid
+    context '内容に問題がない場合' do
+      it '情報を正しく入力すれば登録できる' do
+        expect(@item).to be_valid
+      end
     end
-    it '画像がなければ出品できない' do
-      @item.image = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Image can't be blank")
-    end
-    it '商品名がなければ出品できない' do
-      @item.name = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Name can't be blank")
-    end
-    it '商品の説明がなければ出品できない' do
-      @item.description = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Description can't be blank")
-    end
-    it 'カテゴリーが選択されてなければ出品できない' do
-      @item.category_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Category must be other than 1')
-    end
-    it '商品の状態が選択されてなければ出品できない' do
-      @item.condition_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Condition must be other than 1')
-    end
-    it '配送料の負担について選択されてなければ出品できない' do
-      @item.delivery_fee_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Delivery fee must be other than 1')
-    end
-    it '発送元が選択されてなければ出品できない' do
-      @item.area_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Area must be other than 1')
-    end
-    it '発送までの日数が選択されてなければ出品できない' do
-      @item.day_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Day must be other than 1')
-    end
-    it '価格がなければ出品できない' do
-      @item.price = nil
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank")
-    end
-    it '価格が¥300未満では出品できない' do
-      @item.price = 299
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
-    end
-    it '価格が¥9,999,999以上では出品できない' do
-      @item.price = 10_000_000
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
-    end
-    it '価格は半角数字でなけれ出品できない' do
-      @item.price = '１０００'
-      @item.valid?
-      expect(@item.errors.full_messages).to include('Price is not a number')
+    context '内容に問題がある場合' do
+      it '画像がなければ出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名がなければ出品できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明がなければ出品できない' do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーが選択されてなければ出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
+      end
+      it '商品の状態が選択されてなければ出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition must be other than 1')
+      end
+      it '配送料の負担について選択されてなければ出品できない' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery fee must be other than 1')
+      end
+      it '発送元が選択されてなければ出品できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Area must be other than 1')
+      end
+      it '発送までの日数が選択されてなければ出品できない' do
+        @item.day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Day must be other than 1')
+      end
+      it '価格がなければ出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が¥300未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '価格が¥9,999,999以上では出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it '価格は半角数字でなければ出品できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '価格は半角英数字混合では出品できない' do
+        @item.price = "500aa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '価格は半角英語だけでは出品できない' do
+        @item.price = "abcdef"
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
     end
   end
 end
