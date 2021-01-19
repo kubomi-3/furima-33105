92aca85a5f7936990e0242cf177d6f247e39640c
kubomi-3 <yfhyfh0@gmail.com>
2021-01-19 14:23:45 +0900

    ordersコントローラーの記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,42 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create]
+  def index
+    if !@item.order && @item.user_id != current_user.id
+      @user_order = UserOrder.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      pay_item
+      @user_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:user_order).permit(:zip_code, :area_id, :municipality, :address, :building, :telephone_number)
+    .merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
